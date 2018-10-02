c4352a5798eea3f7c812b83f96f0c6703c741d61
PRATIK SURYAWANSHI <32184670+pratik8064@users.noreply.github.com>
2018-10-01 18:19:58 -0700

    add vue.js file in external and move main js file to src

---
+++ app/src/js/index.js
@@ -0,0 +1,68 @@
+var botui = new BotUI('flixbot');
+var username = '';
+
+function greeting() {
+    botui.message
+        .bot({
+            delay: 1000,
+            loading: true,
+            content: 'Hi! I am Flix Bot.'
+        }).then(function() {
+            botui.message.bot({
+                delay: 1000,
+                loading: true,
+                content: 'What is your name?'
+            }).then(function() {
+                setTimeout(function() {
+                    getName();
+                }, 1000);
+            });
+        })
+}
+
+function getName() {
+    botui.action.text({
+        action: {
+            placeholder: "Your name"
+        }
+    }).then(function(res) {
+        username = res.value;
+        botui.message.add({
+            delay: 1000,
+            loading: true,
+            content: "Hello " + username + ", how can I help you?"
+        })
+    }).then(function() {
+        setTimeout(function() {
+            getQuery();
+        }, 2000);
+
+    })
+}
+
+function getQuery() {
+    botui.action.text({
+        action: {
+            placeholder: 'I want to know about ...'
+        }
+    }).then(function(res) {
+        return botui.message.add({
+            delay: 1000,
+            loading: true,
+            content: "I'm sorry " + username + ", I don't have the data to answer your query."
+        })
+    }).then(function() {
+        return botui.message.add({
+            delay: 1000,
+            loading: true,
+            content: "Can I help you with something else?"
+        });
+    }).then(function() {
+        setTimeout(function() {
+            getQuery();
+        }, 1000);
+    });
+
+}
+
+greeting();
