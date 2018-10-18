4e1544c5ac0fc56d50f11a10df4772e2fe035a70
PRATIK SURYAWANSHI <32184670+pratik8064@users.noreply.github.com>
2018-10-17 22:59:53 -0700

    add api to fetch trending movies from theMovieDb api (Sprint:2, UserStory: #10, Task: #56)

--- server/server.js
+++ server/server.js
@@ -26,14 +26,42 @@ var dummyRequest = {
 // sample server api
 server.get('/dummy', function (req, res) {
     // code to request moviedbapi
+    //data from dialogflow i.e. moviename
     request(dummyRequest, function(error, response, body) {
         if (error) throw error;
         //log to check if result is received. this result is supposed to be sent to dialogflow via res.send()
         //console.log(body);
-        res.send(body);        
+        res.send(response);        
     });
 });
 
+//GET request for getting trending movies from theMovieDb API
+var trendingRequest = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/trending/movie/week",
+    qs: { api_key: "b9ba76892aceca8cadef96bae5ca959b", page: "1" },
+    headers: {
+        //authorization: "Bearer <<access_token>>",
+        "content-type": "application/json;charset=utf-8"
+    },
+    body: {},
+    json: true
+};
+
+// api to call theMovieDb api to get list of trending movies
+server.get('/trending', function (req, res) {
+    request(trendingRequest, function(error, response, body) {
+        if (error) throw error;
+        results = body.results;
+        output = [];    
+        results.forEach(item => {
+            output.push(item.original_title);
+        });
+        result = {};
+        result.trendingMoviesList = output;
+        res.send(result);    
+    });
+});
 
 server.use(function(req, res, next) {
     res.status(404).send("Sorry, not found");
