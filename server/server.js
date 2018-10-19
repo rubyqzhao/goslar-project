0928eaed2fe6f816faf51854a77d643da665d813
Rishabh27795 <rpmodi@asu.edu>
2018-10-18 21:05:58 -0700

    Implement code to call TheMovieDb api to get information related to ratings
    User Story #8
    Task #44

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ server.use(bodyParser.json()); // support json encoded bodies
 server.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 
-// a sample dummy request object with sample url to fetch info about 
+// a sample dummy request object with sample url to fetch info about
 var dummyRequest = {
     method: "GET",
     url: "https://api.themoviedb.org/3/movie/550",
@@ -30,11 +30,31 @@ server.get('/dummy', function (req, res) {
         if (error) throw error;
         //log to check if result is received. this result is supposed to be sent to dialogflow via res.send()
         //console.log(body);
-        res.send(body);        
+        res.send(body);
     });
 });
 
+//GET request for getting review of the movie from theMovieDb API
+var reviewRequest = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/movie/550/reviews",
+    qs: { api_key: "b9ba76892aceca8cadef96bae5ca959b", page: "1" },
+    headers: {
+        //authorization: "Bearer <<access_token>>",
+        "content-type": "application/json;charset=utf-8"
+    },
+    body: {},
+    json: true
+};
 
+// api to call theMovieDb api to get review of the movie
+server.get('/reviews', function (req, res) {
+    request(reviewRequest, function(error, response, body) {
+        if (error) throw error;
+        result = body.results[0].content;
+        res.send(result);
+    });
+});
 server.use(function(req, res, next) {
     res.status(404).send("Sorry, not found");
 });
