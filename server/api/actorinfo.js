39f272e06f7cdc930149f1840bbe0aef1e3c7887
PRATIK SURYAWANSHI <32184670+pratik8064@users.noreply.github.com>
2018-11-19 21:03:00 -0700

    add code to get response and create message for actor info (US #12, Task #97)

---
+++ server/api/actorinfo.js
@@ -0,0 +1,35 @@
+var request = require('request');
+
+var getActorInfoRequest = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/person/",
+    qs: {
+        api_key: "b9ba76892aceca8cadef96bae5ca959b",
+        page: "1"
+    },
+    headers: {
+        //authorization: "Bearer <<access_token>>",
+        "content-type": "application/json;charset=utf-8"
+    },
+    body: {},
+    json: true
+};
+
+function getActorInfoMsg(body, callback) {
+    var msg = body.biography;
+    return msg;
+}
+
+
+function getActorInfo(id, callback) {
+    getActorInfoRequest.url = "https://api.themoviedb.org/3/person/" + id;
+    request(getActorInfoRequest, function (error, response, body) {
+        if (error)
+            throw error;
+        else
+            callback(body);
+    });
+}
+
+exports.getActorInfo = getActorInfo;
+exports.getActorInfoMsg = getActorInfoMsg;
